800fbca09ccdc283fe2f7a4c50fac7c0436ffcf9
Derrick Simpson <derrick.simpson@gmail.com>
2020-05-10 15:53:39 -0400

    commit static assets

---
+++ precache-manifest.b547cc646af483b0cb02e092287e5659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232466ea7fed16f017a3f082071b26f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3cb5f849cdf90a1897f",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ce86cd5e7532ef6488ae",
+    "url": "/static/js/2.f86e092f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.f86e092f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3cb5f849cdf90a1897f",
+    "url": "/static/js/main.0c9508ee.chunk.js"
+  },
+  {
+    "revision": "281d74c4039d65fe3036",
+    "url": "/static/js/runtime-main.ae91b275.js"
+  }
+]);
